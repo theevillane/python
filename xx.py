90b4e777d8d037dbc9138677105d8252cecd66e5
Brian1 <brianochiengonyango2@gmail.con>
2024-10-28 18:20:18 +0300

    Initial commit

---
+++ xx.py
@@ -0,0 +1,11 @@
+x1 = int(input("Enter value of x1: "))
+x2 = int(input("Enter value of x2: "))
+y1 = int(input("Enter value of y1: "))
+y2 = int(input("Enter value of y2: "))
+
+dy = y2 - y1
+dx = x2 - x1
+
+z = dy / dx
+
+print("Your change is:", z)
