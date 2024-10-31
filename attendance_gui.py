78ad40060e08d1216a05e085dce481a11a16c559
Brian1 <brianochiengonyango2@gmail.con>
2024-11-01 00:32:30 +0300

    bg color set to green

--- attendance_gui.py
+++ attendance_gui.py
@@ -5,6 +5,7 @@ class AttendanceRecorder:
     def __init__(self, root):
         self.root = root
         self.root.title("Attendance Recorder")
+        self.root.configure(bg='lightgreen') #background colour
         
         self.attendance_list = self.load_attendance()
         self.admin_passwords = ["admin123", "owandho254", "123456789"]
@@ -70,4 +71,3 @@ if __name__ == "__main__":
     root = tk.Tk()
     app = AttendanceRecorder(root)
     root.mainloop()
-    
