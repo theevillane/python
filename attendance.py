f05690a44be5989edaa4a7999ad2ac4c0fc0eafd
Brian1 <brianochiengonyango2@gmail.con>
2024-11-03 13:05:40 +0300

    User login

--- attendance.py
+++ attendance.py
@@ -1,6 +1,14 @@
+import hashlib
+
+def hash_password(password):
+    """Hash a password for secure storage."""
+    return hashlib.sha256(password.encode()).hexdigest()
+
+
 def record_attendance():
     # Initialize an empty list to store student names
     attendance_list = []
+    users = {}
     
     #Load existing attendance from file if available.
     try:
@@ -11,6 +19,49 @@ def record_attendance():
 
     admin_password = ["admin123", "owandho254", "123456789"] 
 
+    #users(username, password)
+    #users = {"Student1": "password1", "Student2": "password2"}
+
+    #loged_in_user = None
+
+    while True:
+        print("\n1. Sign up")
+        print("2.Sign in")
+        print("3. Exit")
+        choice = input("Choose an option(1-3): ")
+
+        if choice == '1':
+            #user registration
+            username = input("Enter username")
+            if username in users:
+                print("Username already exists. Try a different one")
+            else:
+                password =input("Enter a password: ")
+                users[username] = hash_password(password)
+                print("Registration succesful.")
+
+        elif choice == '2':
+            username = ("Enter your username: ")
+            password = ("Enmter your password: ")
+            hashed_password = hash_password(password)
+
+            if username in users and users[username] == hashed_password:
+                print(f"Welcome, {username}!")
+                logged_in_user = username
+
+
+
+    #while True:
+        #if not loged_in_user:
+            #print("\nPlease login and continue.")
+            #username = input("Enter Username: ")
+            #password = input("Enter password:")
+            #if username in users and users[username] == password:
+             #   loged_in_user = username
+              #  print(f"Welcome, {loged_in_user}!")
+            #else:
+             #   print("Invalid username or password. Try again")
+              #  continue
     while True:
         print("\nAttendance List")
         print("-------------------")
