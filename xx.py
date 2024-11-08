784f2ea27cdbd7c118d9fa700e8bdd79b415a50a
Brian1 <brianochiengonyango2@gmail.con>
2024-11-08 19:46:28 +0300

    update

--- xx.py
+++ xx.py
@@ -8,4 +8,18 @@ dx = x2 - x1
 
 z = dy / dx
 i = dy * dx 
-print("The difference in your curtesian plain is:", z. i)
+print("The difference in your curtesian plain is:", z, i)
+
+def f(x):
+    return x ** 2
+
+x1 = float(input("Enter value of x1: "))
+delta_x = float(input("Enter initial value for delta x (for approximation): "))
+
+derative_approx = (f(x1 + delta_x) - f(x1)) / delta_x
+
+print(f"Approximated derivative at x = {x1} using delta x = {delta_x}: {derative_approx}")
+
+new_delta_x = delta_x / 10
+improved_derivative_approx = (f(x1 + new_delta_x) - f(x1)) / new_delta_x
+print(f"Improved approximation for derivative at x = {x1} using smaller delta x = {new_delta_x}: {improved_derivative_approx}")
