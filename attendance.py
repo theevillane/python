ca134c2d2c1297e21d5f0da59590d91e3a3b5635
Brian <148261369+theevillane@users.noreply.github.com>
2024-11-14 22:54:53 +0300

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -49,7 +49,7 @@ def save_attendance_to_mongo(attendance_list, username):
 
 def register_user(users, admin_users):
     """Register a new user with role and password validation."""
-    ADMIN_CONFIRM_PASSWORD_HASH = hash_password(os.getenv("ADMIN_CONFIRM_PASSWORD", "owandho@254")) #Admin confirmation password for added security
+    ADMIN_CONFIRM_PASSWORD_HASH = hash_password(os.getenv("ADMIN_CONFIRM_PASSWORD", "Kijanamdogo")) #Admin confirmation password for added security
 
     username = input("Enter a username: ").lower()
     if username in users or username in admin_users:
