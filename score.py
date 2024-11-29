52ddf2be84aebce01b0ea59f17dcd5f656db7026
theevillane <brianochiengonyango2@gmail.con>
2024-11-30 00:52:59 +0300

    bonus score

--- score.py
+++ score.py
@@ -17,12 +17,12 @@ def basketball_score_tracker():
         if choice == '1':
             total_score += 2
             total_shots += 1
-            print("Added a two-point shot.")
+            print(f"Two-point shot made! Score is {total_score}.")
         
         elif choice == '2':
             total_score += 3
             total_shots += 1
-            print("Added a three-point shot.")
+            print(f"Three-point shot made!. Total score is{total_score}")
         
         elif choice == '3':
             print(f"Total score: {total_score} points")
@@ -31,15 +31,20 @@ def basketball_score_tracker():
             print(f"Total shots made: {total_shots}")
 
         elif choice == '5':
-            if shot > 3:
-                print("Added ! point")
+            if total_score > 15:
+                total_score += 1
+                print(f"Score is {total_score}")
         
         elif choice == '6':
-            print("Exiting the score tracker.")
-            break
+            confirm_exit = input("Are you sure you want to exit(yes/no): ")
+            if confirm_exit == 'yes':
+                print("Exiting the score tracker.")
+                break
+            else:
+                continue
         
         else:
-            print("Invalid option. Please choose a number between 1 and 5.")
+           print("Invalid option. Please choose a number between 1 and 6.")
 
 # Run
 basketball_score_tracker()
