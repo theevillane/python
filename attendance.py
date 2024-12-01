444cd166d4b0c2122c0e8b96d20c732f9cb887ba
Brian <brianochiengonyango2@gmail.com>
2024-12-01 13:43:48 +0300

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -289,7 +289,7 @@ def record_attendance(users, admin_users):
                         print("3. Delete Student")
                     print("4. View Attendance List")
                     print("5. Save Attendance")
-                    print("6. Back to Main Mwnu")
+                    print("6. Back to Main Menu")
 
                     menu_choice = input("Choose an option: ")
 
